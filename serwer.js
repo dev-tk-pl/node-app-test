600e4dbcc9252fe17380573cf04474046bc7400a
unknown <dev_tk@int.pl>
2023-05-28 19:29:20 +0200

    first commit

---
+++ serwer.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('MY NODE APP');
+});
+
+app.listen(port, () => {
+  console.log(`Serwer jest uruchomiony na porcie ${port}`);
+});
